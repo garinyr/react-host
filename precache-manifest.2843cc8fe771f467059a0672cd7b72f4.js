376027ff3e2f305b21694b16dd45229177f6748b
Garin YR <garinyr92@gmail.com>
2020-08-21 02:23:55 +0700

    Updates

---
+++ precache-manifest.2843cc8fe771f467059a0672cd7b72f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eef73e6c32ac3289279013873902c20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f050830bbe20da00061a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f050830bbe20da00061a",
+    "url": "/static/js/main.ca04deb1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
